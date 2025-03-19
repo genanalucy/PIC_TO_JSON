4e102e9a961b653273ff43f2ac016448c86e2537
genanalucy <1498870503@qq.com>
2025-03-19 17:09:49 +0800

    0319 1.修复 outoflist 2.修复方言按钮全选问题

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class ImageViewerApp:
     def __init__(self, root):
         self.root = root
         self.root.geometry("600x1400")
-        self.root.title("字典json生成0329v2.0.2")
+        self.root.title("字典json生成0319v2.0.2")
 
         # 初始化数据结构
         self.current_data = {
